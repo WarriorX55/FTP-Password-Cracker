37b4d0b45c535225101ef64622e66a05e1468fc5
WarriorX55 <141424663+WarriorX55@users.noreply.github.com>
2024-01-23 14:56:22 +0100

    Update ftp-password-cracker.py

--- ftp-password-cracker.py
+++ ftp-password-cracker.py
@@ -5,24 +5,23 @@ import ftplib
 print(pyfiglet.figlet_format('FTP - Cracker'))
 
 # Take user input as to IP address of the FTP server
-server = input("What is the IP Address of the FTP server: ")
+server = input("[*] What is the IP Address of the FTP server: ")
 print("use => " +server)
 
 # Take user input on the username to attempt to crack
-username = input("What is the username are you trying to crack: ")
+username = input("[*] What is the username are you trying to crack: ")
 print("use => " +username)
 
 # Take user input as to the location and filename of the password list
-passwordlist = input("Select the path of your password list: ")
+passwordlist = input("[*] Select the path of your password list: ")
 print("use => " +passwordlist)
 
 # use this try/except to attempt to connect to the ftp server
 try:
-
     with open(passwordlist, 'r') as pw:
 
         for word in pw:
-            if word is not None:
+            if word is None:
                 word=word.strip('\n')
 
                 try:
@@ -39,4 +38,7 @@ try:
 
 except:
     print("Please check the path of wordlist. Either the file does not exist or the wrong path")
+
+
+
 
